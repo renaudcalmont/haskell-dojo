578f641d0d98421375301e835a5549759d0f4d1a
Renaud Calmont <renaud.calmont@gmail.com>
2023-10-08 15:16:59 +0000

    Haskell Dojo Assignment 1: Types & Expressions

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -1,51 +1,82 @@
+{-# LANGUAGE GADTs #-}
 module TTT.A1 where
 
 import Data.Char (toUpper)
+import System.Directory (XdgDirectory(XdgCache))
+import GHC.IO.Exception (IOErrorType(TimeExpired))
 
 -- Q#01
 
-_SIZE_ = undefined
+_SIZE_ = 3
 
 -- Q#02
 
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ = True
 
 -- Q#03
-
-convertRowIndex = undefined
+convertRowIndex :: Char -> Int
+convertRowIndex i = fromEnum (toUpper i) - 65
 
 -- Q#04
 
-_INVALID_MOVE_ = undefined
+_INVALID_MOVE_ = (-1, -1)
 
 -- Q#05
 
-_SEP_ = undefined
+_SEP_ = ['_', '|', '_']
 
 -- Q#06
 
-data Square
+data Square = X | O | Empty
+  deriving (Show, Eq)
 
 -- Q#07
 
-data GameState
+data GameState where
+  Xwon :: GameState
+  Owon :: GameState
+  Tie :: GameState
+  InProgress :: GameState
+  deriving (Show, Eq)
 
 -- Q#08
 
+type Player = Square
+type Row = [Square]
+type Line = [Square]
+type Board = [Row]
+type Move = (Int, Int)
+
 -- Q#09
 
-getFirstPlayer = undefined
+getFirstPlayer :: Bool -> Player
+getFirstPlayer cf = if cf then X else O
 
-getFirstPlayer_ = undefined
+getFirstPlayer_ cf
+ | cf = X
+ | otherwise = O
 
 -- Q#10
 
-showGameState = undefined
+showGameState :: GameState -> String
+showGameState gs = case gs of
+  Xwon -> "X won the game"
+  Owon -> "O won the game"
+  Tie -> "The game is a tie"
+  InProgress -> "The game is in progress"
 
 -- Q#11
 
-switchPlayer = undefined
+switchPlayer :: Player -> Player
+switchPlayer p
+ | p == X = O
+ | p == O = X
+ | otherwise = Empty 
 
 -- Q#12
 
-showSquare = undefined
+showSquare :: Square -> String
+showSquare s
+ | s == X = "X"
+ | s == O = "O"
+ | otherwise = "_"
